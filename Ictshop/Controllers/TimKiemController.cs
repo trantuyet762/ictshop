89f2a5f00b54a98589d675fc85dcad3eb14cd0d1
trantuyet762 <trantuyet2203@hmail.com>
2023-11-10 19:34:53 +0700

    first commit

---
+++ Ictshop/Controllers/TimKiemController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ictshop.Models;
+
+namespace Ictshop.Controllers
+{
+    public class TimKiemController : Controller
+    {
+        Qlbanhang db = new Qlbanhang();
+        // GET: TimKiem
+        [HttpGet]
+        public ActionResult KQTimKiem(string TuKhoa)
+        {
+            var listSP = db.Sanphams.Where(n => n.Tensp.Contains(TuKhoa));
+            ViewBag.TuKhoa = TuKhoa;
+            return View(listSP.OrderBy(n => n.Tensp));
+        }
+        [HttpPost]
+        public ActionResult LayTuKhoaTimKiem(string TuKhoa)
+        {
+
+            return RedirectToAction("KQTimKiem", new { @TuKhoa = TuKhoa });
+        }
+    }
+}
